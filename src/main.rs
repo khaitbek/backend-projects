72b654d778179efe0f17c6f4d556e92d9b0a3c05
khaitbek <khaitbek2005@gmail.com>
2024-08-25 15:20:12 +0500

    fix: refactor the add_todo function to use unwrap_or_else() instead of inner match hell

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::Write;
+use std::{
+    fs::File,
+    io::{BufReader, Write},
+};
 
 use clap::Parser;
 use rand::prelude::*;
@@ -18,6 +21,8 @@ struct Task {
     completed: bool,
 }
 
+static TASKS_FILE_NAME: &str = "tasks.json";
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     let command = args.command;
@@ -35,53 +40,63 @@ fn add_todo() {
     // get the todo name from the user using a prompt
     // and add it to the tasks.json file
     let mut task_name = String::new();
-    println!("Enter the task name");
-    match std::io::stdin().read_line(&mut task_name) {
-        Ok(_) => {
-            if task_name.trim().len() > 0 {
-                let mut rng = rand::thread_rng();
-                let new_task = Task {
-                    completed: false,
-                    id: rng.gen::<i32>(),
-                    title: task_name.trim().to_string(),
-                };
 
-                let new_task_to_json = serde_json::to_string(&new_task);
-
-                match new_task_to_json {
-                    Ok(task) => {
-                        match std::fs::read_to_string("tasks.json") {
-                            Ok(content) => {
-                                let content_to_json: Result<Vec<Task>, serde_json::Error> =
-                                    serde_json::from_str(&content);
-                                match content_to_json {
-                                    Ok(mut tasks) => {
-                                        tasks.push(new_task);
-                                        let tasks_to_json = serde_json::to_string(&tasks).unwrap();
-                                        let mut file = std::fs::File::create("tasks.json").unwrap();
-                                        file.write(tasks_to_json.as_bytes()).unwrap();
-                                    }
-                                    Err(err) => {
-                                        println!("Error while converting the tasks.json file content to a vector of tasks: {}", err)
-                                    }
-                                }
-                            }
-                            Err(err) => {
-                                println!("Error while reading the tasks.json file: {}", err)
-                            }
-                        }
-                        println!("New task: {}", task)
-                    }
-                    Err(err) => println!("Error occured: {}", err),
-                }
-            }
-        }
-        Err(err) => {
-            println!("Error: {}", err);
-        }
+    while task_name.trim().len() == 0 {
+        // get the todo name from the user
+        println!("Enter the task name");
+        std::io::stdin()
+            .read_line(&mut task_name)
+            .unwrap_or_else(|err| panic!("Error while reading the task name: {err:?}"));
     }
+
+    // get all tasks from the json file
+    let mut tasks = get_all_tasks();
+
+    // create a new task
+    let new_task = Task {
+        completed: false,
+        id: generate_random_id(),
+        title: task_name.trim().to_string(),
+    };
+
+    // add the task to the todos vector
+    tasks.push(new_task);
+
+    // convert the todos vector to json
+    let tasks = serde_json::to_string(&tasks)
+        .unwrap_or_else(|err| panic!("Error while converting the tasks vector to JSON: {err:?}"));
+
+    let mut tasks_file = File::create(TASKS_FILE_NAME)
+        .unwrap_or_else(|err| panic!("Error while reading the {TASKS_FILE_NAME} file: {err:?}"));
+
+    tasks_file.write(tasks.as_bytes()).unwrap_or_else(|err| {
+        panic!(
+            "Error while writing the tasks vector content to the {TASKS_FILE_NAME} file: {err:?}"
+        )
+    });
+}
+
+fn generate_random_id() -> i32 {
+    let mut rng = rand::thread_rng();
+
+    rng.gen::<i32>()
 }
 
 fn list_todo() {}
 
 fn remove_todo() {}
+
+fn get_all_tasks() -> Vec<Task> {
+    let tasks_file = File::open(TASKS_FILE_NAME)
+        .unwrap_or_else(|err| panic!("Error while reading the {TASKS_FILE_NAME} file: {err:?}"));
+
+    let reader = BufReader::new(tasks_file);
+
+    let tasks: Vec<Task> = serde_json::from_reader(reader).unwrap_or_else(|err| {
+        panic!(
+            "Error while converting the contents of the {TASKS_FILE_NAME} file into JSON: {err:?}"
+        )
+    });
+
+    tasks
+}
